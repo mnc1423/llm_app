2eb4c873de4c68aad340fff441243c7f6e672735
mnc1423 <runegod123@naver.com>
2025-01-23 15:09:59 +0900

    temp update

--- pages/2_Database.py
+++ pages/2_Database.py
@@ -6,19 +6,27 @@ def read_pdf(file):
     pdf_reader = PyPDF2.PdfReader(file)
     num_pages = len(pdf_reader.pages)
     content = ""
-    for page_num in range(num_pages):
+    for page_num in range(min(1, num_pages)):
         content += pdf_reader.pages[page_num].extract_text()
     return content
 
 
 st.title("Database")
 
-# File upload
-file = st.file_uploader("Upload a PDF file", type="pdf")
+upload_col, db_col = st.columns([2, 1])
 
-if file is not None:
-    # Read PDF and extract text
-    content = read_pdf(file)
-    # Display the content
-    st.subheader("Extracted Text:")
-    st.markdown(content)
+
+with upload_col:
+    # File upload
+    st.container()
+    file = st.file_uploader("Upload a PDF file", type="pdf")
+
+    if file is not None:
+        # Read PDF and extract text
+        content = read_pdf(file)
+        # Display the content
+        st.container()
+        st.subheader("Extracted Text:")
+        st.markdown(content)
+with db_col:
+    pass
