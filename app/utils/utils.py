9a99130f80579bce607116e1377e5dab6d7f2d7e
mnc1423 <runegod123@naver.com>
2025-05-13 10:36:36 +0900

    code cleanup

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -111,13 +111,10 @@ def debounce_replicate_run(llm, prompt, max_len, temperature, top_p, API_TOKEN):
     global last_call_time
     print("last call time: ", last_call_time)
 
-    # Get the current time
     current_time = time.time()
 
-    # Calculate the time elapsed since the last call
     elapsed_time = current_time - last_call_time
 
-    # Check if the elapsed time is less than the debounce interval
     if elapsed_time < debounce_interval:
         print("Debouncing")
         return "Hello! You are sending requests too fast. Please wait a few seconds before sending another request."
